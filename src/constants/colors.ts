4a45f2395a6a6321c14a82d2c1b48de36744cca8
daifAB <abd.daif96@gmail.com>
2023-11-01 10:02:07 +0100

    first commit

---
+++ src/constants/colors.ts
@@ -0,0 +1,5 @@
+export const chartColors = {
+  strokeColor: '#C0C0BB',
+  mainTextColor: '#635F5D',
+  secondaryTextColor: '#8E8883',
+};
---
+++ src/types/chartsDataTypes.tsx
@@ -0,0 +1,11 @@
+export interface BarChartData {
+  label: string;
+  value: number;
+}
+
+export interface ChartMargin {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+}
